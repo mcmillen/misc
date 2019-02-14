0940b9e355042f1b4adc6b50c6c8210662a4a72c
Colin McMillen <me@famicol.in>
2019-02-14 02:17:57 +0000

    Reformat license text slightly.

--- ohnosay.py
+++ ohnosay.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-# Copyright 2019 Colin McMillen (https://twitter.com/mcmillen).
+# MIT License
+#
+# Copyright (c) 2019 Colin McMillen
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
